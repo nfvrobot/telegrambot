f3c8767d4de36e18dd25facece5c6b8841824f48
nfvrobot <fraggedbynfv@ggmail.com>
2021-02-05 20:16:15 +0300

    Initial commit

---
+++ src/main/java/com/chernyavsky/telgram/App.java
@@ -0,0 +1,14 @@
+package com.chernyavsky.telgram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.telegram.telegrambots.ApiContextInitializer;
+
+@SpringBootApplication
+public class App {
+
+    public static void main(String[] args) {
+        ApiContextInitializer.init();
+        SpringApplication.run(App.class, args);
+    }
+}
---
+++ src/main/java/com/chernyavsky/telgram/bot/Bot.java
@@ -0,0 +1,40 @@
+package com.chernyavsky.telgram.bot;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+@Component
+public class Bot extends TelegramLongPollingBot {
+
+    @Value("${bot.name}")
+    private String name;
+
+    @Value("${bot.token}")
+    private String token;
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        Long chatId = update.getMessage().getChatId();
+        SendMessage message = new SendMessage();
+        message.setChatId(chatId);
+        message.setText("Hi!");
+        try {
+            execute(message);
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }
+    @Override
+    public String getBotUsername() {
+        return null;
+    }
+
+    @Override
+    public String getBotToken() {
+        return null;
+    }
+}
