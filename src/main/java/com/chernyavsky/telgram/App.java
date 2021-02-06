b6cfafe39e8560cb265ba5b13baf983132028c04
nfvrobot <fraggedbynfv@ggmail.com>
2021-02-06 13:14:45 +0300

    update commit

--- src/main/java/com/chernyavsky/telgram/App.java
+++ src/main/java/com/chernyavsky/telgram/App.java
@@ -2,13 +2,11 @@ package com.chernyavsky.telgram;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.telegram.telegrambots.ApiContextInitializer;
 
 @SpringBootApplication
 public class App {
 
     public static void main(String[] args) {
-        ApiContextInitializer.init();
         SpringApplication.run(App.class, args);
     }
 }
--- src/main/java/com/chernyavsky/telgram/bot/Bot.java
+++ src/main/java/com/chernyavsky/telgram/bot/Bot.java
@@ -1,14 +1,20 @@
 package com.chernyavsky.telgram.bot;
 
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
+import javax.annotation.PostConstruct;
+
 @Component
 public class Bot extends TelegramLongPollingBot {
+
+    private static final Logger log = Logger.getLogger(Bot.class);
 
     @Value("${bot.name}")
     private String name;
@@ -18,23 +24,34 @@ public class Bot extends TelegramLongPollingBot {
 
     @Override
     public void onUpdateReceived(Update update) {
-        Long chatId = update.getMessage().getChatId();
-        SendMessage message = new SendMessage();
-        message.setChatId(chatId);
-        message.setText("Hi!");
-        try {
-            execute(message);
-        } catch (TelegramApiException e) {
-            e.printStackTrace();
+        log.debug("Receive new Update. updateID: " + update.getUpdateId());
+        if (update.hasMessage()) {
+            Message message = update.getMessage();
+            SendMessage response = new SendMessage();
+            Long chatId = message.getChatId();
+            response.setChatId(String.valueOf(chatId));
+            String text = "Hello there!";
+            response.setText(text);
+            try {
+                execute(response);
+                log.info("Sent message " + text + " to: " + chatId);
+            } catch (TelegramApiException e) {
+                log.error("Failed to send message due to error: " + e.getMessage());
+            }
         }
     }
     @Override
     public String getBotUsername() {
-        return null;
+        return name;
     }
 
     @Override
     public String getBotToken() {
-        return null;
+        return token;
+    }
+
+    @PostConstruct
+    public void start() {
+        log.info("name: " + name + " token: " + token);
     }
 }
